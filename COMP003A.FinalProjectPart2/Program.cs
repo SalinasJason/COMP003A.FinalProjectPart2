0ee0e35b4d692661561a3a123d742d2b59f79425
SalinasJason <jasonsalinas770@gmail.com>
2025-02-25 14:32:59 -0800

    Added menu and switch statments

--- COMP003A.FinalProjectPart2/Program.cs
+++ COMP003A.FinalProjectPart2/Program.cs
@@ -1,10 +1,49 @@
-﻿namespace COMP003A.FinalProjectPart2
+﻿using COMP003A.FinalProjectPart2;
+
+namespace COMP003A.FinalProjectPart2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+
+            int choice = 0;
+            Console.WriteLine("Welcome to the Library System");
+
+            Console.WriteLine("Main Menu: ");
+            Console.WriteLine("1. Add Physical Book");
+            Console.WriteLine("2. Add eBook");
+            Console.WriteLine("3. View Book");
+            Console.WriteLine("4. Edit Book");
+            Console.WriteLine("5. Delete Book");
+            Console.WriteLine("6. Exit");
+            Console.Write("Enter your choice: ");
+            choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1: 
+                    Console.WriteLine("Enter book title");
+                    break;
+                case 2:
+                    Console.WriteLine();
+                    break;
+                case 3:
+                    Console.WriteLine();
+                    break;
+                case 4:
+                    Console.WriteLine();
+                    break;
+                case 5:
+                    Console.WriteLine();
+                    break;
+                case 6:
+                    Console.WriteLine();
+                    break;
+                default:
+                    Console.WriteLine();
+                    break ;
+            }
         }
     }
 }
